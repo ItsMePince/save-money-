a23d54179123fd8c188a209a9985fedb6665cac3
pinceme <wissawachit15@gmail.com>
2025-11-15 15:00:00 +0700

    fix test cypress

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
     e2e: {
-        baseUrl: "http://localhost:3000", // เปลี่ยนตาม dev server ของคุณ
+        baseUrl: "https://my-app.local", // เปลี่ยนตาม dev server ของคุณ
         video: true,
         screenshotOnRunFailure: true,
         setupNodeEvents(on, config) {
